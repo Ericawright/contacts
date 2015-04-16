82a5b9bb9871d4496d7be3a9cdd9ae599fd5ebd9
Erica Wright <ericaewright@gmail.com>
2015-04-16 01:25:06 +0000

    clean up class methods

--- phone_number.rb
+++ phone_number.rb
@@ -1,14 +1,5 @@
 class PhoneNumber < ActiveRecord::Base
   belongs_to :contact, dependent: :destroy
 end
-  # attr_accessor :type, :digits
 
-  # def initialize(type, digits)
-  #   @type = type
-  #   @digits = digits
-  # end
-
-  # def to_s
-  #   " #{@type}: #{@digits}"
-  # end
 
